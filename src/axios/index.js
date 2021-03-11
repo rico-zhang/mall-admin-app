1bb96b3d424006a41ef3eaf439682522c41b6615
rico-zhang <1425618127@qq.com>
2021-03-11 23:51:36 +0800

    登录9-1

---
+++ src/axios/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://mallapi.duyiedu.com/',
+});
+instance.interceptors.request.use((request) => {
+  console.log(request);
+  // if (request.url.includes(''))
+  return request;
+}, (error) => Promise.reject(error));
+
+instance.interceptors.response.use((response) => {
+  console.log(response);
+  if (response.data.status === 'fail') return Promise.reject(response.data.msg);
+  return response.data.data;
+}, (error) => Promise.reject(error));
+
+export default instance;
