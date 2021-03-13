2b1dab36d01727856250dd7f118f33917351ebf4
rico-zhang <1425618127@qq.com>
2021-03-13 19:54:12 +0800

    新增商品页面

--- src/views/page/ProductAdd.vue
+++ src/views/page/ProductAdd.vue
@@ -1,9 +1,67 @@
 <template>
-  <div class="product-add">商品新增</div>
+  <div class="product-detail">
+    <a-steps :current="current" class="product-step">
+      <a-step v-for="item in steps" :key="item.title" :title="item.title" />
+    </a-steps>
+    <div class="steps-content">
+      <component :is="steps[current].cmpName" @next="next" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import BasicDetail from '@/components/BasicDetail.vue';
+import SaleDetail from '@/components/SaleDetail.vue';
 
+export default {
+  data() {
+    return {
+      current: 0,
+      steps: [
+        {
+          title: '填写基本商品信息',
+          cmpName: 'BasicDetail',
+        },
+        {
+          title: '填写商品销售信息',
+          cmpName: 'SaleDetail',
+        },
+      ],
+    };
+  },
+  methods: {
+    next(form) {
+      this.current += 1;
+      console.log(form);
+    },
+    prev() {
+      this.current -= 1;
+    },
+  },
+  components: {
+    BasicDetail,
+    SaleDetail,
+  },
 };
 </script>
+<style scoped lang="less">
+.product-detail {
+  .product-step {
+    width: 50%;
+    margin: 20px auto;
+  }
+  .steps-content {
+    margin-top: 16px;
+    border: 1px dashed #e9e9e9;
+    border-radius: 6px;
+    background-color: #fafafa;
+    min-height: 200px;
+    text-align: center;
+    padding-top: 80px;
+  }
+
+  .steps-action {
+    margin-top: 24px;
+  }
+}
+</style>
